b8a692654491dda970a19db1a2ce71f9eae66e7d
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2025-03-02 18:59:12 +0530

    fix permission signal

--- vault/signals.py
+++ vault/signals.py
@@ -1,1 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+from vault.models import PrivateFile, FilePermission
+
+
+@receiver(post_save, sender=PrivateFile)
+def create_file_permission(sender, **kwargs):
+    if kwargs['created']:
+        FilePermission.objects.create(file=kwargs['instance'])
