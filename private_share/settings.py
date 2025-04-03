adb85fef97b6c71f944f60738ec3f5bdef10fc81
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2025-04-04 01:44:37 +0530

    change page size

--- private_share/settings.py
+++ private_share/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 12,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
     ],
